2ddeedcf2dd2ba07c9d87b3d7bad5cc0c5688a8b
Choi Seongwon <vlflalektzja@naver.com>
2024-08-12 02:32:42 +0900

    feat: eureka 서버 구성

--- EurekaServer/src/main/java/com/sparta/msa_exam/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/sparta/msa_exam/EurekaServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class MsaExamApplication {
+@SpringBootApplication
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsaExamApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
--- EurekaServer/src/test/java/com/sparta/msa_exam/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/sparta/msa_exam/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sparta.msa_exam.msa_exam;
+package com.sparta.msa_exam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsaExamApplicationTests {
+class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
